3f6ca355b3aadd61dfff599b4e9e348c05d5b0f7
Ben Lei <ben181231@gmail.com>
2021-01-07 02:10:15 +0900

    Add the gradient background

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,33 @@
 module Main exposing (..)
 
-import Browser
-import Html exposing (text)
+import Css exposing (..)
+import Css.Transitions exposing (background)
+import Html as HtmlCore
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (css)
 
-main : Html.Html msg
-main = text "hello world"
+
+main : HtmlCore.Html msg
+main =
+    toUnstyled <|
+        fullScreenView []
+
+
+fullScreenView : List (Html msg) -> Html msg
+fullScreenView content =
+    div
+        [ css
+            [ position absolute
+            , backgroundColor <| hex "#302266"
+            , backgroundImage <|
+                linearGradient
+                    (stop <| hex "#221847")
+                    (stop <| hex "#302266")
+                    []
+            , top <| px 0
+            , left <| px 0
+            , right <| px 0
+            , bottom <| px 0
+            ]
+        ]
+        content
